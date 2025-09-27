0e74263efdfdfc7dd6653b934b1f1344d81eb9de
iamvidhya-git <vidhyanandsrv@gmail.com>
2025-09-27 22:10:17 +0530

    apply js on menuIcon

--- README.md
+++
@@ -1,2 +0,0 @@
-# DigiMedia
-For Good Sleep
---
+++ script.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector("nav .fa-bars");
+let menu = document.querySelector("body menu");
+
+menuIcon.addEventListener("click", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
+menuIcon.addEventListener("mouseover", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
