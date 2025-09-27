ebc30bdacab712f1aba9d293efd3180128b4a401
iamvidhya-git <vidhyanandsrv@gmail.com>
2025-09-27 23:02:21 +0530

    add href variables in anchor

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Code line 2 to 21, only for menuIcon click and mouseover event to display and hide the menu respectively.
+
 let menuIcon = document.querySelector("nav .fa-bars");
 let menu = document.querySelector("body menu");
 
@@ -15,4 +17,19 @@ menuIcon.addEventListener("mouseover", function () {
   } else {
     menu.style.display = "block";
   }
+});
+
+// Code line 2 to 21, only for menuIcon click and mouseover event to display and hide the menu respectively.
+// JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Yahaan hum variable banayenge
+    var linkURL = 'https://facebook.com';
+    
+    // Sabhi anchor tags ko select karke unka href attribute update karenge
+    var links = document.querySelectorAll('.home_anchor');
+    
+    links.forEach(function(link) {
+        link.setAttribute('href', linkURL);
+        link.setAttribute('target', '_blank'); // New tab mein kholne ke liye
+    });
 });
