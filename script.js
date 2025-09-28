0564592e10f17c25b47dda9ff5e82af696f69217
iamvidhya-git <vidhyanandsrv@gmail.com>
2025-09-28 10:16:57 +0530

    remove_menufiles or remove script part

--- script.js
+++ script.js
@@ -19,17 +19,5 @@ menuIcon.addEventListener("mouseover", function () {
   }
 });
 
-// Code line 2 to 21, only for menuIcon click and mouseover event to display and hide the menu respectively.
-// JavaScript code
-document.addEventListener('DOMContentLoaded', function() {
-    // Yahaan hum variable banayenge
-    var linkURL = 'https://facebook.com';
-    
-    // Sabhi anchor tags ko select karke unka href attribute update karenge
-    var links = document.querySelectorAll('.home_anchor');
-    
-    links.forEach(function(link) {
-        link.setAttribute('href', linkURL);
-        link.setAttribute('target', '_blank'); // New tab mein kholne ke liye
-    });
-});
+
+
