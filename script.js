82ba4dffc1d4cb5c050ebf3420ab20f96c33786b
iamvidhya-git <vidhyanandsrv@gmail.com>
2025-09-30 19:23:57 +0530

    Yes updates

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 // Code line 2 to 21, only for menuIcon click and mouseover event to display and hide the menu respectively.
 
-let menuIcon = document.querySelector("nav .fa-bars");
-let menu = document.querySelector("body menu");
+let menuIcon = document.querySelector("nav>.fa-bars");
+let menu = document.querySelector("body>menu");
 
-menuIcon.addEventListener("click", function () {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+menuIcon.addEventListener('click', function() {
+    if (menu.style.width === '0px') {
+        menu.style.width = '150px';
+    } else {
+        menu.style.width = '0px';
+    }
 });
 
-menuIcon.addEventListener("mouseover", function () {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+
+menuIcon.addEventListener('mouseover', function() {
+    if (menu.style.width === '0px') {
+        menu.style.width = '150px';
+    } else {
+        menu.style.width = '0px';
+    }
 });
 
+
+// line 6 to  12 और 15 से 21 के जगह आप इस कोड को उपयोग कर सकते है पर कोई एक रहने दे । 
+
+// menuIcon.addEventListener('click', toggleMenu);
+// menuIcon.addEventListener('mouseover', toggleMenu);
+
+// function toggleMenu() {
+//     if (menu.style.width === '0px' || menu.style.width === '') {
+//         menu.style.width = '150px';
+//     } else {
+//         menu.style.width = '0px';
+//     }
+// }
