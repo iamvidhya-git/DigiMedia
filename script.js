a4611d75e77de7264a5456fdc509a9f3bd602c55
iamvidhya-git <vidhyanandsrv@gmail.com>
2025-10-17 10:54:37 +0530

    yes add animation

--- script.js
+++ script.js
@@ -11,20 +11,3 @@ menuIcon.addEventListener('click', function() {
     }
 });
 
-// menuIcon.addEventListener('mousemove', function() {
-//         menu.style.width = 'fit-content';
-// });
-
-
-// line 6 to  12 और 15 से 21 के जगह आप इस कोड को उपयोग कर सकते है पर कोई एक रहने दे । 
-
-// menuIcon.addEventListener('click', toggleMenu);
-// menuIcon.addEventListener('mouseover', toggleMenu);
-
-// function toggleMenu() {
-//     if (menu.style.width === '0px' || menu.style.width === '') {
-//         menu.style.width = '150px';
-//     } else {
-//         menu.style.width = '0px';
-//     }
-// }
